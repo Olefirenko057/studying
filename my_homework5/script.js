ca85c6e8d92308d916d97273abce4e23cad6bc54
Olefirenko057 <117396653+Olefirenko057@users.noreply.github.com>
2023-04-21 17:20:52 +0200

    Add files via upload

---
+++ my_homework5/script.js
@@ -0,0 +1,47 @@
+// Написать программу, которая через prompt считывает число и выводит в консоль число равное 10% от введенного числа
+
+// const number = +prompt("Enter a number");
+// const findTenPercent = number * 0.1;
+// console.log(findTenPercent);
+
+
+
+
+// Написать программу, которая получает два числа и выводит наименьшее
+
+// const digit1 = +prompt("Enter a number");
+// const digit2 = +prompt("Enter yet another number");
+
+// if (digit1 < digit2) {
+//     console.log(digit1)
+// } else if (digit1 > digit2) {
+//     console.log(digit2)
+// } 
+
+
+
+// Написать программу, которая считывает через prompt число и выводит одно из следующих сообщений: ‘Число меньше 100’, ‘Число больше 100’ или ‘Число равно 100
+
+// const number = +prompt("Enter a number");
+
+// if (number < 100) {
+//     console.log('Число меньше 100')
+// } 
+// else if (number == 100) {
+//     console.log('Число равно 100')
+// } 
+// else {
+//     console.log('Число больше 100')
+// }
+
+
+
+// Написать программу, которая запрашивает у пользователя его имя и возраст (в годах) и выводит в консоль сообщение ‘Hello, <name>’, если пользователь совершеннолетний, или ‘Hi, <name>’, если пользователь несовершеннолетний
+
+// const username = prompt("What is your name?");
+// const userAge = +prompt("How old are you?");
+// const minAdultAge = 18;
+// const greeting = userAge >= minAdultAge ? `Hello, ${username}` :  `Hi, ${username}`;
+// console.log(greeting);
+
+
